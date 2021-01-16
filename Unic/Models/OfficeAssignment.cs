e6fa8437a050283f1e58b775d3e5f8ef3be5924b
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-16 17:07:05 +0200

    Added the OfficeAssignment model.

---
+++ Unic/Models/OfficeAssignment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unic.Models
+{
+    public class OfficeAssignment
+    {
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public Instructor Instructor { get; set; }
+    }
+}
