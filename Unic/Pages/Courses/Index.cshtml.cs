e04334ef0b4cb90ea8d33295cb33d9abfe3c3c79
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-17 14:50:57 +0200

    Scaffolded the courses page.

---
+++ Unic/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Unic.Data;
+using Unic.Models;
+
+namespace Unic.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Unic.Data.SchoolContext _context;
+
+        public IndexModel(Unic.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department).ToListAsync();
+        }
+    }
+}
