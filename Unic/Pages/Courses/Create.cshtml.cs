cf4fb976e09d5388bd1f01b4d95c5dff3aa751b5
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-19 21:44:51 +0200

    Used TryUpdateModelAsync to prevent over posting.

--- Unic/Pages/Courses/Create.cshtml.cs
+++ Unic/Pages/Courses/Create.cshtml.cs
@@ -10,36 +10,42 @@ using Unic.Models;
 
 namespace Unic.Pages.Courses
 {
-    public class CreateModel : PageModel
+    public class CreateModel : DepartmentNamePageModel
     {
-        private readonly Unic.Data.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public CreateModel(Unic.Data.SchoolContext context)
+        public CreateModel(SchoolContext context)
         {
             _context = context;
         }
 
         public IActionResult OnGet()
         {
-        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            PopulateDepartmentsDropDownList(_context);
             return Page();
         }
 
         [BindProperty]
         public Course Course { get; set; }
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var emptyCourse = new Course();
+
+            if (await TryUpdateModelAsync<Course>(emptyCourse,
+                "course",
+                s => s.CourseID,
+                s => s.DepartmentID,
+                s => s.Title,
+                s => s.Credits))
             {
-                return Page();
+                _context.Add(emptyCourse);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("./Index");
             }
 
-            _context.Courses.Add(Course);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            PopulateDepartmentsDropDownList(_context, emptyCourse.DepartmentID);
+            return Page();
         }
     }
 }
