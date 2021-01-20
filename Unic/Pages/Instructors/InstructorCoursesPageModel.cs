c54b61d2c40f5061a36155e60e49bf9751c8be7a
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-20 22:11:52 +0200

    Created the base which is going to be used in order to display the courses as checkboxes within edit and create instructor's page and will update the selected courses for the instructor.

---
+++ Unic/Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Unic.Data;
+using Unic.Models;
+using Unic.Models.SchoolViewModels;
+
+namespace Unic.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseData> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context, Instructor instructor)
+        {
+            var allCourses = context.Courses;
+            var instructorCourses = new HashSet<int>(instructor.CourseAssignments.Select(ca => ca.CourseID));
+
+            AssignedCourseDataList = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseData
+                {
+                    CourseId = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+
+        public void UpdateInstructorCourses(SchoolContext context, Instructor instructorToUpdate,
+            string[] selectedCourses)
+        {
+            if (selectedCourses == null)
+            {
+                instructorToUpdate.CourseAssignments = new List<CourseAssignment>();
+                return;
+            }
+
+            var selectedCoursesHS = new HashSet<string>(selectedCourses);
+            var instructorCourses = new HashSet<int>(instructorToUpdate.CourseAssignments
+                                                    .Select(ca => ca.CourseID));
+
+            foreach (var course in context.Courses)
+            {
+                if (selectedCoursesHS.Contains(course.CourseID.ToString()))
+                {
+                    if (!instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.CourseAssignments.Add(
+                            new CourseAssignment
+                            {
+                                InstructorID = instructorToUpdate.ID,
+                                CourseID = course.CourseID
+                            });
+                    }
+                }
+                else
+                {
+                    if (instructorCourses.Contains(course.CourseID))
+                    {
+                        CourseAssignment courseToRemove = instructorToUpdate
+                                                          .CourseAssignments
+                                                          .SingleOrDefault(ca => ca.CourseID == course.CourseID);
+
+                        context.Remove(courseToRemove);
+                    }
+                }
+            }
+        }
+    }
+}
