f9c4ff10d19e5b999ec44832b91282a924f8ca92
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-20 22:30:49 +0200

    Updated Instructor's edit page to use InstructorCoursesPageModel in order to display and update instructor's department checkboxes and it's office location.

--- Unic/Pages/Instructors/Edit.cshtml.cs
+++ Unic/Pages/Instructors/Edit.cshtml.cs
@@ -1,21 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Unic.Data;
 using Unic.Models;
 
 namespace Unic.Pages.Instructors
 {
-    public class EditModel : PageModel
+    public class EditModel : InstructorCoursesPageModel
     {
-        private readonly Unic.Data.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public EditModel(Unic.Data.SchoolContext context)
+        public EditModel(SchoolContext context)
         {
             _context = context;
         }
@@ -30,48 +25,61 @@ namespace Unic.Pages.Instructors
                 return NotFound();
             }
 
-            Instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.ID == id);
+            Instructor = await _context.Instructors
+                                       .Include(i => i.OfficeAssignment)
+                                       .Include(i => i.CourseAssignments)
+                                            .ThenInclude(ca => ca.Course)
+                                       .AsNoTracking()
+                                       .FirstOrDefaultAsync(m => m.ID == id);
 
             if (Instructor == null)
             {
                 return NotFound();
             }
+
+            PopulateAssignedCourseData(_context, Instructor);
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id, string[] selectedCourses)
         {
-            if (!ModelState.IsValid)
+            if (id == null)
             {
+                return NotFound();
+            }
+
+            var instructorToUpdate = await _context.Instructors
+                                                   .Include(i => i.OfficeAssignment)
+                                                   .Include(i => i.CourseAssignments)
+                                                        .ThenInclude(ca => ca.Course)
+                                                   .FirstOrDefaultAsync(i => i.ID == id);
+
+            if (instructorToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            if (await TryUpdateModelAsync<Instructor>(
+                instructorToUpdate,
+                "Instrcutor",
+                i => i.FirstMidName,
+                i => i.LastName,
+                i => i.HireDate,
+                i => i.OfficeAssignment))
+            {
+                if (string.IsNullOrEmpty(instructorToUpdate.OfficeAssignment?.Location))
+                {
+                    instructorToUpdate.OfficeAssignment = null;
+                }
+
+                UpdateInstructorCourses(_context, instructorToUpdate, selectedCourses);
+                await _context.SaveChangesAsync();
                 return Page();
             }
 
-            _context.Attach(Instructor).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!InstructorExists(Instructor.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
-        }
-
-        private bool InstructorExists(int id)
-        {
-            return _context.Instructors.Any(e => e.ID == id);
+            UpdateInstructorCourses(_context, instructorToUpdate, selectedCourses);
+            PopulateAssignedCourseData(_context, instructorToUpdate);
+            return Page();
         }
     }
 }
