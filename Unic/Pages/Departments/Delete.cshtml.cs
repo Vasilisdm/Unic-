d19ad7d56a12acddccd60ace51d1cd3f1d4216f6
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-23 20:51:08 +0200

    Modified page to detect concurrency conflicts when the entity has changed after it was fetched.

--- Unic/Pages/Departments/Delete.cshtml.cs
+++ Unic/Pages/Departments/Delete.cshtml.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,17 +9,18 @@ namespace Unic.Pages.Departments
 {
     public class DeleteModel : PageModel
     {
-        private readonly Unic.Data.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public DeleteModel(Unic.Data.SchoolContext context)
+        public DeleteModel(SchoolContext context)
         {
             _context = context;
         }
 
         [BindProperty]
         public Department Department { get; set; }
+        public string ConcurrencyErrorMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int? id, bool? concurrencyError)
         {
             if (id == null)
             {
@@ -30,31 +28,37 @@ namespace Unic.Pages.Departments
             }
 
             Department = await _context.Departments
-                .Include(d => d.Administrator).FirstOrDefaultAsync(m => m.DepartmentID == id);
+                .Include(d => d.Administrator)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.DepartmentID == id);
 
-            if (Department == null)
+            if (concurrencyError.GetValueOrDefault())
             {
-                return NotFound();
+                ConcurrencyErrorMessage = "The record you attempted to delete "
+                  + "was modified by another user after you selected delete. "
+                  + "The delete operation was canceled and the current values in the "
+                  + "database have been displayed. If you still want to delete this "
+                  + "record, click the Delete button again.";
             }
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
+                if (await _context.Departments.AnyAsync(d => d.DepartmentID == id))
+                {
+                    _context.Departments.Remove(Department);
+                    await _context.SaveChangesAsync();
+                }
+
+                return RedirectToPage("./Index");
             }
-
-            Department = await _context.Departments.FindAsync(id);
-
-            if (Department != null)
+            catch (DbUpdateConcurrencyException ex)
             {
-                _context.Departments.Remove(Department);
-                await _context.SaveChangesAsync();
+                return RedirectToPage("./Delete", new { concurrencyError = true, id });
             }
-
-            return RedirectToPage("./Index");
         }
     }
 }
